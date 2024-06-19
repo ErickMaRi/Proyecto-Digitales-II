5aaa49cda12ba12a3e5bf8fdc0acfd0c7d80361d
Brenda Romero <brenda.romero@ucr.ac.cr>
2024-06-18 18:55:49 -0600

    Actualización de la documentación y makefile

--- MDIO/MDIO_tb.v
+++ MDIO/MDIO_tb.v
@@ -1,3 +1,24 @@
+/************************************************************
+                    Universidad de Costa Rica
+                 Escuela de Ingenieria Electrica
+                            IE0523
+                   Circuitos Digitales 2
+
+                        MDIO_tb.v
+
+Autores: 
+        Brenda Romero Solano  brenda.romero@ucr.ac.cr
+
+Fecha: [fecha de modificación] 
+    
+*********************************************************** */ 
+
+
+//! @title Testbench dell controlador del MDIO
+/**
+ * Descripción pendiente.
+ */
+ 
 `include "controller/controller.v"
 `include "peripheral/peripheral.v"
 `timescale 1ns / 1ps
@@ -5,20 +26,51 @@
 module MDIO_tb;
 
 // Declaración de señales del MDIO
-reg clk, reset;
-// Otras señales del MDIO...
+// Inputs
+reg CLK;
+reg RESET;
+reg MDIO_START;
+reg [31:0] T_DATA;
+
+// Outputs
+wire [15:0] RD_DATA;
+wire DATA_RDY;
+wire MDC;
+wire MDIO_OE;
+wire MDIO_OUT;
+
+wire [4:0] ADDR; 
+wire [15:0] WR_DATA;   
+wire MDIO_DONE;
+wire WR_STB;
+wire MDIO_IN_PERIPHERAL;
+
 
 // Instancias de los módulos controlador y periférico
-controller controller_inst (
-    .clk(clk),
-    .reset(reset)
-    // Otras señales del controlador...
+mdio_controller controller_inst (
+        .CLK(CLK),
+        .RESET(RESET),
+        .MDIO_START(MDIO_START),
+        .T_DATA(T_DATA),
+        .MDIO_IN(MDIO_IN_PERIPHERAL),
+        .RD_DATA(RD_DATA),
+        .DATA_RDY(DATA_RDY),
+        .MDC(MDC),
+        .MDIO_OE(MDIO_OE),
+        .MDIO_OUT(MDIO_OUT)
 );
 
 peripheral peripheral_inst (
-    .clk(clk),
-    .reset(reset)
-    // Otras señales del periférico...
+    .RESET(RESET),
+    .RD_DATA(RD_DATA),
+    .MDC(MDC),
+    .MDIO_OE(MDIO_OE),
+    .MDIO_OUT(MDIO_OUT),
+    .ADDR(ADDR),
+    .WR_DATA(WR_DATA),
+    .MDIO_DONE(MDIO_DONE),
+    .WR_STB(WR_STB),
+    .MDIO_IN(MDIO_IN_PERIPHERAL)
 );
 
 // Inicialización de señales
@@ -26,8 +78,8 @@ initial begin
     $dumpfile("sim.vcd");
     $dumpvars(0, MDIO_tb);
 
-    clk = 0;
-    reset = 1;
+    CLK = 0;
+    RESET = 1;
     // Inicialización de otras señales...
 
     // Generación de estímulos para el MDIO...
@@ -36,6 +88,6 @@ initial begin
 end
 
 // Generación de reloj
-always #5 clk = ~clk;
+always #5 CLK = ~CLK;
 
 endmodule
